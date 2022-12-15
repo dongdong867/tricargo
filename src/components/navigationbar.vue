c91da0a2583d9d2e6eaa0448e710905ed078787b
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-16 04:29:05 +0800

    fix: Fix navigationbar layout damage

--- src/components/navigationbar.vue
+++ src/components/navigationbar.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="sticky bottom-0 shrink-0">
-		<div class="sticky top-0 left-0 right-0 flex place-content-around text-xs m-2">
-			<nuxt-link to="record" class="flex flex-col place-items-center">
+	<div class="sticky w-[100vw] h-[8vh] bottom-0 shrink-0">
+		<div class="h-[8vh] top-0 left-0 right-0 flex place-content-around place-items-center text-xs">
+			<nuxt-link to="record" class="h-[8vh] flex flex-col place-content-center place-items-center">
 				<img :src="History" />
 				<div>History</div>
 			</nuxt-link>
-			<nuxt-link to="account" class="flex flex-col place-items-center">
+			<nuxt-link to="account" class="h-[8vh] flex flex-col place-content-center place-items-center">
 				<img :src="User" />
 				<div>User</div>
 			</nuxt-link>
