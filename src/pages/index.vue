981f39af0664f4b4fbe01235e9f2c79edd1b2dbb
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-19 07:46:09 +0800

    fix: Remove useless component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,14 +5,11 @@
 			:class="pageloaded ? ['opacity-100'] : ['opacity-0']"
 		>
 			<div id="map" class="max-w-lg h-[92vh]"></div>
-			<div class="absolute w-[100vw] h-[45vh]" :class="park ? ['top-[47vh]'] : ['top-[100vh]']">
-				<Parking :parkinglot="selectedParkinglot" :space="spacedata" />
-			</div>
 			<div
-				class="absolute w-[100vw] h-[45vh]"
-				:class="calculate ? ['top-[50vh] opacity-100'] : ['top-[100vh] opacity-0']"
+				class="relative w-[100vw] h-[45vh]"
+				:class="park ? ['-top-[45vh] opacity-100'] : [' opacity-0']"
 			>
-				<Calculate />
+				<Parking :parkinglot="selectedParkinglot" :space="spacedata" />
 			</div>
 		</div>
 		<div
