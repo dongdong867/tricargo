c9aaf380fb798a44f501dae58965091c11ccb0a7
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-27 16:31:16 +0800

    feat: Add selected color to navigationbar

--- src/components/navigationbar.vue
+++ src/components/navigationbar.vue
@@ -1,17 +1,26 @@
 <template>
 	<div class="sticky w-[100vw] h-[8vh] bottom-0 shrink-0">
 		<div class="h-[8vh] top-0 left-0 right-0 flex place-content-around place-items-center text-xs">
-			<nuxt-link to="record" class="h-[8vh] flex flex-col place-content-center place-items-center">
-				<img :src="History" />
-				<div>History</div>
+			<nuxt-link
+				to="/record"
+				class="h-[8vh] aspect-square flex flex-col place-content-center place-items-center"
+			>
+				<img :src="HistoryHovered" v-if="route.name === 'record'" />
+				<img :src="History" v-else />
 			</nuxt-link>
-			<a href="/" class="h-[8vh] flex flex-col place-content-center place-items-center">
-				<img :src="Home" />
-				<div>home</div>
+			<a
+				href="/"
+				class="h-[8vh] aspect-square flex flex-col place-content-center place-items-center"
+			>
+				<img :src="HomeHovered" v-if="route.name === '/'" />
+				<img :src="Home" v-else />
 			</a>
-			<nuxt-link to="account" class="h-[8vh] flex flex-col place-content-center place-items-center">
-				<img :src="User" />
-				<div>User</div>
+			<nuxt-link
+				to="/account"
+				class="h-[8vh] aspect-square flex flex-col place-content-center place-items-center"
+			>
+				<img :src="UserHovered" v-if="route.name === 'account'" />
+				<img :src="User" v-else />
 			</nuxt-link>
 		</div>
 	</div>
@@ -20,4 +29,9 @@
 import History from '~/assets/icons/navigationbar/history.svg?url'
 import User from '~/assets/icons/navigationbar/user.svg?url'
 import Home from '~/assets/icons/navigationbar/home.svg?url'
+import HomeHovered from '~/assets/icons/navigationbar/home-hovered.svg?url'
+import UserHovered from '~/assets/icons/navigationbar/user-hovered.svg?url'
+import HistoryHovered from '~/assets/icons/navigationbar/history-hovered.svg?url'
+
+const route = useRoute()
 </script>
