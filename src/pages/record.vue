2cfcc5875dcc5eac7281bc23c71effa16894430c
James <jam219888@gmail.com>
2022-12-27 19:02:39 +0800

    change the format of parking time

--- src/pages/record.vue
+++ src/pages/record.vue
@@ -2,9 +2,9 @@
 	<div class="font-bold pt-5 pb-2 mr-2 ml-2 text-3xl border-b-2 border-b-black  font-sans">History</div>
 	<div>
 		<div class="flex justify-between bg-history-base-gray my-2 mx-3 rounded " v-for="history in historys">
+
 			<div>
 				<div class="text-xl font-bold ml-2 pt-2">{{ history.parkinglot }}</div>
-				<div class="text-sm ml-2 text-history-dark-gray">{{ history.parkingDate }}</div>
 				<div class="text-sm ml-2 pb-2 text-history-dark-gray">{{ history.parkingTime }}</div>
 			</div>
 			
@@ -18,9 +18,20 @@
 const historys = [
 	{
 		parkinglot: 'OO Road No.XX',
-		parkingDate: 'XXXX/XX/XX',
-		parkingTime: '00:00 - XX:XX',
+		parkingTime: 'XXXX/XX/XX 00:00 - XXXX/XX/XX XX:XX',
 		totalPrice: '$XXX'
+	},
+	{
+		parkinglot: '木柵路3段220號',
+		parkingDate: '2022/12/17',
+		parkingTime: '2022/12/17 00:00 - 2022/12/17 18:25',
+		totalPrice: '$100'
+	},
+	{
+		parkinglot: '保儀路平面停車場(保儀路157號對面)',
+		parkingDate: '2022/12/18',
+		parkingTime: '2022/12/17 09:00 - 2023/01/02 14:25',
+		totalPrice: '$1032'
 	}
 ]
 </script>
