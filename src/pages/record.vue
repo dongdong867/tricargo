ac7744130f2c7277ec3bdf5677fedbbf0a597891
pupu <110306060@g.nccu.edu.tw>
2022-12-29 19:05:35 +0800

    histiory

--- src/pages/record.vue
+++ src/pages/record.vue
@@ -20,22 +20,43 @@
 </template>
 <script setup>
 const historys = [
+
 	{
-		parkinglot: 'OO Road No.XX',
-		parkingTime: 'XXXX/XX/XX 00:00 - XXXX/XX/XX XX:XX',
-		totalPrice: '$XXX'
+		parkinglot: 'City Parking城市車旅停車場(光輝路92號)',
+		parkingDate: '2022/12/20',
+		parkingTime: '2022/12/20 17:00 - 2023/12/20 18:30',
+		totalPrice: '$90'
 	},
 	{
-		parkinglot: '木柵路3段220號',
+		parkinglot: '文山區下崙路9-3號',
+		parkingDate: '2022/12/18',
+		parkingTime: '2022/12/18 18:50 - 2023/12/14 22:10',
+		totalPrice: '$30'
+	},
+	{
+		parkinglot: '保儀路平面停車場(保儀路157號對面)',
+		parkingDate: '2022/12/17s',
+		parkingTime: '2022/12/17 09:00 - 2023/01/02 14:25',
+		totalPrice: '$120'
+	},
+	{
+		parkinglot: '文山區木柵路3段220號',
 		parkingDate: '2022/12/17',
 		parkingTime: '2022/12/17 00:00 - 2022/12/17 18:25',
 		totalPrice: '$100'
 	},
 	{
-		parkinglot: '保儀路平面停車場(保儀路157號對面)',
-		parkingDate: '2022/12/18',
-		parkingTime: '2022/12/17 09:00 - 2023/01/02 14:25',
-		totalPrice: '$1032'
-	}
+		parkinglot: '文山區行政中心便民停車場',
+		parkingDate: '2022/12/14',
+		parkingTime: '2022/12/14 14:50 - 2023/12/14 15:30',
+		totalPrice: '$30'
+	},
+	{
+		parkinglot: '松山區健康路',
+		parkingDate: '2022/12/10',
+		parkingTime: '2022/12/10 11:34 - 2023/12/10 13:30',
+		totalPrice: '$40'
+	},
+	
 ]
 </script>
