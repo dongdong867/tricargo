31272d1a63eca6d591105b4b92d94f1fc931c224
gino885 <ginozxcv885@gmail.com>
2022-12-30 17:19:34 +0800

    set layout

--- src/pages/account/index.vue
+++ src/pages/account/index.vue
@@ -1,32 +1,37 @@
 <template>
 	<div class="container min-w-full">
-		<div class="mt-10">
+		<div class="relative top-20 ">
 			<div class="flex justify-center">
 				<img :src="personalImage" />
 			</div>
-			<div class="flex flex-col space-y-10">
-				<div class="animate-fade-in-down mb-1 text-center text-lg text-sky-600 font-semibold">
+		</div>
+				<div class="flex-col space-y-40 ">
+				<div class="relative top-24 animate-fade-in-down mb-1 text-center text-lg text-sky-600 font-semibold">
 					藍色妖姬你不愛 非在渣男身上找存在
 				</div>
+			
 
-				<div class="flex flex-col space-y-20">
-					<div class="mb-4 flex justify-center">
+				<div class="flex-col space-y-16 ">
+					<div class="flex justify-center">
 						<img :src="spareBalance" />
 					</div>
-				</div>
-				<div class="flex-col space-y-3 justify-center grid gap-2 content-end">
+				
+				
+				<div class="flex-col space-y-4 justify-center relative bottom-2">
 					<div class="flex justify-center">
 						<nuxt-link to="/account/wallet">
 							<img :src="wallet" />
 						</nuxt-link>
 					</div>
+				
+					<div class="flex justify-center "> <img :src="setting" /> </div>
+						
+					<div class="flex justify-center"> <img :src="help" /> </div>
 				</div>
-
-				<img :src="setting" />
-
-				<img :src="help" />
 			</div>
-		</div>
+			</div>
+			
+			
 	</div>
 </template>
 <script setup>
