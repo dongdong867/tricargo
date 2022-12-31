b135ccef620ac19d39a4cba1445f25778789d6ba
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-31 14:04:54 +0800

    refactor: rebuild record page and

--- src/pages/record.vue
+++ src/pages/record.vue
@@ -1,20 +1,25 @@
 <template>
-	<div class="font-bold pt-5 pb-2 mr-2 ml-2 text-3xl border-b-2 border-b-black  font-sans">History</div>
-	<div>
-		<div class="flex justify-between bg-history-base-gray my-2 mx-3 rounded " v-for="history in historys">
+	<div class="p-[3vw] font-bold">
+		<div class="font-bold mt-[2vh] text-3xl border-b-2 border-b-black">History</div>
+		<div>
+			<div
+				class="flex place-content-between place-items-center px-[3vw] bg-history-base-gray mt-[1vh] rounded-[10px]"
+				v-for="history in historys"
+			>
+				<div class="w-[65vw]">
+					<div class="text-xl leading-normal">{{ history.parkinglot }}</div>
+					<div class="text-sm text-history-dark-gray">
+						{{ history.parkingStartTime }}
+					</div>
+					<div class="text-sm text-history-dark-gray">
+						{{ history.parkingEndingTime }}
+					</div>
+				</div>
 
-			<div>
-				<div class="text-xl font-bold ml-2 pt-2">{{ history.parkinglot }}</div>
-				<div class="text-sm ml-2 pb-2 text-history-dark-gray">{{ history.parkingStartTime }}</div>
-				<div class="text-sm ml-2 pb-2 text-history-dark-gray">{{ history.parkingEndingTime }}</div>
-
+				<div class="text-base-blue text-[40px]">{{ history.totalPrice }}</div>
 			</div>
-			
-			<div class=" text-base-blue mt-2 mr-5 text-[40px] font-bold">{{ history.totalPrice }}</div>
 		</div>
 	</div>
-	
-	
 </template>
 <script setup>
 const historys = [
@@ -22,7 +27,7 @@ const historys = [
 		parkinglot: '木柵路3段220號',
 		parkingDate: '2022/12/20',
 		parkingStartTime: '2022/12/20 00:00 - ',
-		parkingEndingTime:'2022/12/20 18:25',
+		parkingEndingTime: '2022/12/20 18:25',
 		totalPrice: '$40'
 	},
 	{
@@ -43,7 +48,7 @@ const historys = [
 		parkinglot: '文山區行政中心便民停車場',
 		parkingDate: '2022/12/13',
 		parkingStartTime: '2022/12/13 14:50 - ',
-		parkingEndingTime: '2023/13 15:20',
+		parkingEndingTime: '2023/12/13 15:20',
 		totalPrice: '$30'
 	},
 	{
