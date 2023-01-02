26e01af33bbe1ced7c86e7632951e6e4692de883
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-01-02 13:29:23 +0800

    style: Set cursor style

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 	<Transition name="page" mode="out-in">
-		<div class="max-w-lg h-[100vh] mx-auto">
+		<div class="max-w-lg h-[100vh] mx-auto cursor-default">
 			<div class="h-[92vh] overflow-y-scroll bg-scroll">
 				<NuxtPage />
 			</div>
