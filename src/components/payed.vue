8b5ba0805ce4f7b9436600fcfbb46c5689d9406d
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-01-02 13:23:23 +0800

    feat: Add loading background and payment process

---
+++ src/components/payed.vue
@@ -0,0 +1,59 @@
+<template>
+	<div
+		class="w-[90vw] h-[60vh] bg-base-grey bg-opacity-80 rounded-[30px] flex flex-col place-content-center place-items-center text-white font-bold text-xl space-y-[3vh]"
+	>
+		<div class="text-5xl">扣款成功</div>
+		<div class="w-[85%] h-[15%] bg-white rounded-[20px] flex place-content-end place-items-center">
+			<div class="text-base-red text-5xl p-[4vw]">-$20</div>
+		</div>
+		<div class="w-[70vw] space-y-[2vh]">
+			<div>明細</div>
+			<div>
+				<div>{{ parkinglot.area }}</div>
+				<div>停車地點: {{ parkinglot.name }}</div>
+				<div>時間: 1分鐘</div>
+			</div>
+			<div>
+				<div>折扣: 0元</div>
+				<div>共計: 20元</div>
+			</div>
+		</div>
+		<div
+			class="px-[20vw] py-[2vh] rounded-full bg-light-red text-base-red text-5xl cursor-pointer"
+			@click="$emit('confirmButtonClicked')"
+		>
+			確認
+		</div>
+	</div>
+</template>
+
+<script setup>
+const props = defineProps({
+	parkinglot: {
+		area: {
+			type: String,
+			require: true
+		},
+		name: {
+			type: String,
+			require: true
+		},
+		address: {
+			type: String,
+			require: true
+		},
+		tel: {
+			type: String,
+			require: true
+		},
+		serviceTime: {
+			type: String,
+			require: true
+		},
+		payex: {
+			type: String,
+			require: true
+		}
+	}
+})
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,7 +16,6 @@
 				:class="park ? ['-top-[40vh]'] : ['top-0']"
 			>
 				<Parking
-					class=""
 					:parkinglot="selectedParkinglot"
 					:space="spacedata"
 					:timerRunning="timerRunning"
@@ -32,12 +31,19 @@
 		>
 			<ParkedError @closeParkedError="parkedError = !parkedError" />
 		</div>
+		<div v-if="payed == true" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[60%]">
+			<Payed :parkinglot="selectedParkinglot" @confirmButtonClicked="paymentFinished" />
+		</div>
 		<div
-			class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col place-items-center"
-			:class="pageloaded ? ['opacity-0'] : ['opacity-100']"
+			class="absolute w-screen max-w-lg h-screen top-0 bg-[url('~/assets/icons/loading-background.svg?url')] bg-[length:100vw_auto]"
+			:class="pageloaded ? ['opacity-0 -z-50'] : ['opacity-100']"
 		>
-			<img :src="Icon" />
-			<div class="text-2xl font-extrabold leading-[0.8]">LOADING...</div>
+			<div
+				class="w-max h-[90vh] m-auto flex flex-col place-items-center place-content-center drop-shadow-[0_0_8px_rgba(255,255,255,0.8)]"
+			>
+				<img :src="Icon" />
+				<div class="text-2xl font-extrabold leading-[0.8]">LOADING...</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -58,6 +64,7 @@ const timerRunning = ref(false)
 const time = computed(() => formatTime())
 const parked = ref(false)
 const parkedError = ref(false)
+const payed = ref(false)
 
 let selectedParkinglot = new Parkinglot()
 let timerCount = ref(600)
@@ -106,6 +113,7 @@ const timerStop = () => {
 const removeParked = async () => {
 	await new Promise((resolve) => setTimeout(resolve, 60000))
 	parked.value = false
+	payed.value = true
 }
 
 const formatTime = () => {
@@ -124,4 +132,6 @@ const parkedHandler = () => {
 	parked.value = true
 	removeParked()
 }
+
+const paymentFinished = () => (payed.value = false)
 </script>
